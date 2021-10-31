0dfe9c827d9e34b55d0827f049dcdfeb13a32ce5
richie <richie.kwon1@gmail.com>
2021-10-31 14:17:39 +0000

    debug setting

--- src/main.js
+++ src/main.js
@@ -1,14 +1,19 @@
 // @ts-check
 // formatting >> prettier
-console.log('hello world')
+// console.log('hello world')
 
-// Linting: ESLint
-console.log('hello world')
+// // Linting: ESLint
+// console.log('hello world')
 
 // eslint-disable-next-line no-console
 console.log('hello world')
 
 // @ts-check 는 맨 위에
 const someString = 'hi'
-const result = Math.log(someString)
+
+const yera = 8
+
+// @ts-ignore
+const result = Math.log(yera)
 console.log(result)
+console.log(someString)
