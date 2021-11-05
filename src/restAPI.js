87664480618c3e795ea61830e7ea1535e2379f0f
richie <richie.kwon1@gmail.com>
2021-11-05 20:16:19 +0000

    ts

--- src/restAPI.js
+++ src/restAPI.js
@@ -13,8 +13,14 @@ POST/posts
 
 const server = http.createServer((req, res) => {
   if (req.url === '/posts' && req.method === 'GET') {
+    res.statusCode = 200
+    res.end('OK')
   } else if (req.url === 'posts/:id') {
+    res.statusCode = 200
+    res.end('OK')
   } else if (req.url === 'posts' && req.method === 'POST') {
+    res.statusCode = 200
+    res.end('OK')
   } else {
     res.statusCode = 404
     res.end('Not found')
---
+++ src/types.js
@@ -0,0 +1,10 @@
+// @ts-check
+
+/**
+ * @typeof Post
+ * @typeof {string} id
+ * @typeof {string} title
+ * @typeof {string} content
+ **
+ *
+ */
